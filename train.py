01347e65701d9a41f158596130e3f9586456b9c4
Tamimah Zulafa <tamimah.zna@gmail.com>
2019-11-04 17:09:19 +0700

    Update train.py

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import numpy as np
 import prepare_data
+import matplotlib.pyplot as plt
 import models
 import argparse
 import sys
@@ -10,91 +11,75 @@ from nltk import word_tokenize
 from keras.applications.vgg16 import preprocess_input
 from keras.preprocessing import image
 from keras.models import load_model
-
-def extract_image_features(img_path):
-	model = models.VGG_16('weights/vgg16_weights_th_dim_ordering_th_kernels.h5')
-	img = image.load_img(img_path,target_size=(224,224))
-	x = image.img_to_array(img)
-	x = np.expand_dims(x,axis=0)
-	x = preprocess_input(x)
-	last_layer_output = K.function([model.layers[0].input,K.learning_phase()],
-		[model.layers[-1].input])
-	features = last_layer_output([x,0])[0]
-	return features
-
-def preprocess_question(question):
-	word_idx = ebd.load_idx()
-	tokens = word_tokenize(question)
-	seq = []
-	for token in tokens:
-		seq.append(word_idx.get(token,0))
-	seq = np.reshape(seq,(1,len(seq)))
-	return seq
-
-def generate_answer(img_path, question, model):
-	img_features = extract_image_features(img_path)
-	seq = preprocess_question(question)
-	x = [img_features, seq]
-	probabilities = model.predict(x)[0]
-	answers = np.argsort(probabilities[:1000])
-	top_answers = [prepare_data.top_answers[answers[-1]],
-		prepare_data.top_answers[answers[-2]],
-		prepare_data.top_answers[answers[-3]]]
-	
-	return top_answers
-
+from sklearn.model_selection import train_test_split
 
 def main():
 	K.set_image_dim_ordering('th')
-	model_path = 'weights/model_1load.h5'
+	model_path = 'weights/model_1e10.h5'
 
-	"""print('Loading questions ...')
-	questions_train = prepare_data.get_questions_matrix('train')
+	print('Loading questions ...')
+	questions_traintest = prepare_data.get_questions_matrix('train')
 	questions_val = prepare_data.get_questions_matrix('val')
 	print('Loading answers ...')
-	answers_train = prepare_data.get_answers_matrix('train')
+	answers_traintest = prepare_data.get_answers_matrix('train')
 	answers_val = prepare_data.get_answers_matrix('val')
 	print('Loading image features ...')
-	img_features_train = prepare_data.get_coco_features('train')
+	img_features_traintest = prepare_data.get_coco_features('train')
 	img_features_val = prepare_data.get_coco_features('val')
 	print('Creating model ...')
 	
 	model = models.vis_lstm()
+
+	questions_train, questions_test = train_test_split(questions_traintest, test_size = 0.2, random_state = 0)
+	answers_train, answer_test = train_test_split(answers_traintest, test_size=0.2, random_state = 0)
+	img_features_train, img_features_test = train_test_split(img_features_traintest, test_size = 0.2, random_state = 0)
+
 	X_train = [img_features_train, questions_train]
 	X_val = [img_features_val, questions_val]
+	X_test = [img_features_test, questions_test]
 
 	model.compile(optimizer='adam',
 		loss='categorical_crossentropy',
 		metrics=['accuracy'])
 
-	model.fit(X_train,answers_train,
+	history = model.fit(X_train,answers_train,
 		epochs=10,
 		batch_size=200,
-		validation_data=(X_val,answers_val),
-		verbose=1)
+		validation_data=(X_val,answers_val), verbose=1)
 
-	model.save(model_path)"""
-	print('---------------Testing---------------')
-	model = models.vis_lstm()
-	model.load_weights(model_path)
-	image = "examples/COCO_val2014_000000000136.jpg"
-	question = "Which animal is this?"
-	print(question)
-	top_answers = generate_answer(image, question, model)
-	print('Top answers: %s, %s, %s.' %(top_answers[0],top_answers[1],top_answers[2]))
-
-	image = "examples/COCO_val2014_000000000073.jpg"
-	question = "What is this?"
-	print(question)
-	top_answers = generate_answer(image, question, model)
-	print('Top answers: %s, %s, %s.' %(top_answers[0],top_answers[1],top_answers[2]))
-
-	image = "examples/COCO_val2014_000000000073.jpg"
-	question = "How many bicycle in this image?"
-	print(question)
-	top_answers = generate_answer(image, question, model)
-	print('Top answers: %s, %s, %s.' %(top_answers[0],top_answers[1],top_answers[2]))
+	model.save(model_path)
+	
+	# hitung nilai akurasi dan loss test
+	score = model.evaluate(X_test,answer_test)
+	print('Accuracy : ', score[1]*100, '%')
+	print('Loss : ', score[0]*100, '%')
+	
+	# plot nilai akurasi training & validasi
+	acc = history.history['acc']
+	val_acc = history.history['val_acc']
+ 
+	epochs = range(len(acc))+1
+ 	
+	plt.figure()
+	plt.plot(epochs, acc, 'b', label='Training acc')
+	plt.plot(epochs, val_acc, 'r', label='Validation acc')
+	plt.ylabel('Accuracy')
+	plt.xlabel('Epoch')
+	plt.title('Training and validation accuracy')
+	plt.legend()
+	plt.savefig('accvaluese10.png')
+	
+	# plot nilai loss training & validasi
+	loss = history.history['loss']
+	val_loss = history.history['val_loss']
+	plt.figure()
+	plt.plot(epochs, loss, 'b', label='Training loss')
+	plt.plot(epochs, val_loss, 'r', label='Validation loss')
+	plt.ylabel('Loss')
+	plt.xlabel('Epoch')
+	plt.title('Training and validation loss')
+	plt.legend()
+	plt.savefig('lossvaluese10.png')
 	
 
 if __name__ == '__main__':main()
-
