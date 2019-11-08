b398a3d97bb8cb6916dbaa2fbaf9f72ef8601a77
Tamimah Zulafa <tamimah.zna@gmail.com>
2019-11-08 11:45:11 +0700

    Update train.py

--- train.py
+++ train.py
@@ -18,47 +18,50 @@ def main():
 	model_path = 'weights/model_1e10.h5'
 
 	print('Loading questions ...')
-	questions_traintest = prepare_data.get_questions_matrix('train')
+	questions_train = prepare_data.get_questions_matrix('train')
 	questions_val = prepare_data.get_questions_matrix('val')
 	print('Loading answers ...')
-	answers_traintest = prepare_data.get_answers_matrix('train')
+	answers_train = prepare_data.get_answers_matrix('train')
 	answers_val = prepare_data.get_answers_matrix('val')
 	print('Loading image features ...')
-	img_features_traintest = prepare_data.get_coco_features('train')
+	img_features_train = prepare_data.get_coco_features('train')
 	img_features_val = prepare_data.get_coco_features('val')
 	print('Creating model ...')
 	
 	model = models.vis_lstm()
 
-	questions_train, questions_test = train_test_split(questions_traintest, test_size = 0.2, random_state = 0)
+	'''questions_train, questions_test = train_test_split(questions_traintest, test_size = 0.2, random_state = 0)
 	answers_train, answer_test = train_test_split(answers_traintest, test_size=0.2, random_state = 0)
-	img_features_train, img_features_test = train_test_split(img_features_traintest, test_size = 0.2, random_state = 0)
+	img_features_train, img_features_test = train_test_split(img_features_traintest, test_size = 0.2, random_state = 0)'''
 
 	X_train = [img_features_train, questions_train]
 	X_val = [img_features_val, questions_val]
-	X_test = [img_features_test, questions_test]
+	#X_test = [img_features_test, questions_test]
 
 	model.compile(optimizer='adam',
 		loss='categorical_crossentropy',
 		metrics=['accuracy'])
 
 	history = model.fit(X_train,answers_train,
-		epochs=10,
+		epochs=25,
 		batch_size=200,
 		validation_data=(X_val,answers_val), verbose=1)
 
-	model.save(model_path)
+	#model.save(model_path)
+	model.save_weights('weights/modele25_weights.h5')
+	with open('weights/modele25_architecture.json', 'w') as f:
+		f.write(model.to_json())
 	
 	# hitung nilai akurasi dan loss test
-	score = model.evaluate(X_test,answer_test)
-	print('Accuracy : ', score[1]*100, '%')
-	print('Loss : ', score[0]*100, '%')
+	#score = model.evaluate(X_test,answer_test)
+	#print('Accuracy : ', score[1]*100, '%')
+	#print('Loss : ', score[0]*100, '%')
 	
 	# plot nilai akurasi training & validasi
 	acc = history.history['acc']
 	val_acc = history.history['val_acc']
  
-	epochs = range(len(acc))+1
+	epochs = range(len(acc))
  	
 	plt.figure()
 	plt.plot(epochs, acc, 'b', label='Training acc')
@@ -67,7 +70,7 @@ def main():
 	plt.xlabel('Epoch')
 	plt.title('Training and validation accuracy')
 	plt.legend()
-	plt.savefig('accvaluese10.png')
+	plt.savefig('acce25.png')
 	
 	# plot nilai loss training & validasi
 	loss = history.history['loss']
@@ -79,7 +82,7 @@ def main():
 	plt.xlabel('Epoch')
 	plt.title('Training and validation loss')
 	plt.legend()
-	plt.savefig('lossvaluese10.png')
+	plt.savefig('losse25.png')
 	
 
 if __name__ == '__main__':main()
